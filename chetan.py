c4442f5bc87fdab68ce10beb17b8f8fae5c923dc
ChetanKomal <76816799+ChetanKomal@users.noreply.github.com>
2021-05-28 19:45:10 +0530

    Add files via upload

---
+++ chetan.py
@@ -0,0 +1,12 @@
+import turtle as q 
+t = q.Turtle()
+q.title("My Name")
+q.bgcolor("black")
+t.up(),t.bk(470),t.down()
+t.pencolor("red")
+t.forward(50),t.backward(50),t.right(90),t.forward(100),t.left(90),t.forward(50),t.up()
+t.forward(50),t.pendown(),t.left(90),t.forward(100),t.right(180),t.forward(50),t.left(90),t.forward(50),t.left(90),t.forward(50),t.right(180),t.forward(100),t.left(90),t.up()
+t.forward(50),t.pendown(),t.left(90),t.forward(100),t.right(90),t.forward(50),t.left(180),t.forward(50),t.left(90),t.forward(50),t.left(90),t.forward(50),t.left(180),t.forward(50),t.left(90),t.forward(50),t.left(90),t.forward(50),t.up()
+t.forward(100),t.pendown(),t.left(90),t.forward(100),t.right(90),t.forward(50),t.left(180),t.forward(100),t.left(180),t.forward(50),t.right(90),t.forward(100),t.left(90),t.up()
+t.forward(100),t.pendown(),t.left(90),t.forward(100),t.right(90),t.forward(100),t.right(90),t.forward(100),t.left(180),t.forward(50),t.left(90),t.forward(100),t.left(180),t.forward(100),t.right(90),t.forward(50),t.left(90),t.up()
+t.forward(50),t.down(),t.left(90),t.forward(100),t.right(145),t.forward(120),t.left(145),t.forward(100)
